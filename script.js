cbd96ce3f502ff3a987269f7d33917878af9357a
Matt <faymatt96@gmail.com>
2018-01-02 21:53:17 -0500

    Add colors

---
+++ script.js
@@ -0,0 +1,3 @@
+document.body.style.backgroundColor = "#333";
+document.getElementsByClassName("d-header clearfix")[0].style.backgroundColor = "#444";
+document.getElementsByTagName("a").style.color = "white";
