0255d445d1658b8b34697cb02fb5c5afb56e1752
Matt <faymatt96@gmail.com>
2018-01-02 22:32:14 -0500

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # FreeCodeCamp-Forum Theme
 
+Take it easy on your eyes, get yourself a dark theme!
+
 ![Forum Theme](https://github.com/username1001/fcc-forum-theme/blob/master/theme.png)
+
+## Installation
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.body.style.backgroundColor = "#333";
 document.getElementsByClassName("d-header clearfix")[0].style.backgroundColor = "#444";
-document.getElementsByTagName("a").style.color = "white";
+
+document.body.style.color = "white";
