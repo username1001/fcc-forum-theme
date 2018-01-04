d6db95fdd9bf23e81a4361dc4a94765d1d9ddd7a
Matt <faymatt96@gmail.com>
2018-01-04 13:36:50 -0500

    Remove commented code

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// When page renders more threads, the links convert back to original color:
-// (function targetLinks(color) {
-//   var links = document.getElementsByTagName("a");
-//   for (var i = 0; i < links.length; i++) {
-//     if (links[i].href) {
-//       links[i].style.color = color;
-//     }
-//   }
-// })("#DBB62D");
-
 document.body.style.backgroundColor = "#333";
 document.getElementsByClassName("d-header clearfix")[0].style.backgroundColor =
   "#444";
