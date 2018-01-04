7e0273a628909e8f377e7b67367503bc4476a5fc
Matt <faymatt96@gmail.com>
2018-01-04 15:36:24 -0500

    Add color to reply section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.body.style.backgroundColor = "#333";
 document.getElementsByClassName("d-header clearfix")[0].style.backgroundColor =
   "#444";
+
+document.getElementById("reply-control").style.backgroundColor = "#555";
